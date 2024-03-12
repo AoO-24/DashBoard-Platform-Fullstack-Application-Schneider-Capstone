532a5cf12b6a22e9333082cd8aba5db3b529a1c8
Badger-Jay <jy3287@columbia.edu>
2024-03-12 16:28:02 -0500

    Update the Comments

--- Frontend/front/src/App.js
+++ Frontend/front/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
-// import components
+// 导入组件
 import Forum from './components/Forum';
 import CreatePost from './components/Forum/CreatePost';
 import CommunitySupport from './components/CommunitySupport';
@@ -22,20 +22,22 @@ import DestinationConfirmation from './components/DestinationConfirmation';
 import DestinationSuccess from './components/DestinationSuccess';
 import MusicPlayer from './components/MusicPlayer/index.jsx';
 
-
-// Define the main App component
+// 定义主要的App组件
 function App() {
   return (
     <Router>
       <Routes>
+        {/* 定义主页路由 */}
         <Route path="/" element={
           <div className="App">
             <header className="App-header">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/f/f7/Schneider_National_logo.svg" alt="Schneider Logo" className="app-logo"/>
+              {/* 应用的Logo和欢迎标题 */}
+              <img src="https://upload.wikimedia.org/wikipedia/commons/f/f7/Schneider_National_logo.svg" alt="Schneider Logo" className="app-logo"/>
               <h1>Welcome to Schneider Truck</h1>
             </header>
             <nav className="App-nav">
-           <Link to="/destination" className="nav-button">Enter Your Destination</Link>
+              {/* 导航链接，使用<Link>组件实现路由跳转 */}
+              <Link to="/destination" className="nav-button">Enter Your Destination</Link>
               <Link to="/report" className="nav-button">Report Time</Link>
               <Link to="/weather" className="nav-button">Weather Today</Link>
               <Link to="/about_us" className="nav-button">About US</Link>
@@ -46,30 +48,29 @@ function App() {
             </nav>
           </div>
         } />
-          <Route path="/forum" element={<Forum />} />
-          <Route path="/forum/create" element={<CreatePost />} />
-          <Route path="/about_us" element={<AboutUs />} />
-          <Route path="/destination" element={<DestinationForm />} />
-          <Route path="/destination/confirmation" element={<DestinationConfirmation />} />
-          <Route path="/destination/success" element={<DestinationSuccess />} />
-          <Route path="/weather" element={<Weather />} />
-          <Route path="/report" element={<Report />} />
-          <Route path="/report/6hour" element={<SixHourReport />} />
-          <Route path="/report/8hour" element={<EightHourReport />} />
-          <Route path="/report/9hour" element={<NineHourReport />} />
-          <Route path="/community_tips" element={<CommunityTips />} />
-          <Route path="/driving_summary" element={<DrivingSummary />} />
-          <Route path="/feel_unwell" element={<FeelUnwell />} />
-          <Route path="/summary_question" element={<SummaryQuestion />} />
-          <Route path="/appointment_success" element={<AppointmentSuccess />} />
-          <Route path="/summary_question" element={<SummaryQuestion />} />
-          <Route path="/community_support" element={<CommunitySupport />} />
-          <Route path="/music_player" element={<MusicPlayer />} />
+        {/* 路由到各个组件 */}
+        <Route path="/forum" element={<Forum />} />
+        <Route path="/forum/create" element={<CreatePost />} />
+        <Route path="/about_us" element={<AboutUs />} />
+        <Route path="/destination" element={<DestinationForm />} />
+        <Route path="/destination/confirmation" element={<DestinationConfirmation />} />
+        <Route path="/destination/success" element={<DestinationSuccess />} />
+        <Route path="/weather" element={<Weather />} />
+        <Route path="/report" element={<Report />} />
+        <Route path="/report/6hour" element={<SixHourReport />} />
+        <Route path="/report/8hour" element={<EightHourReport />} />
+        <Route path="/report/9hour" element={<NineHourReport />} />
+        <Route path="/community_tips" element={<CommunityTips />} />
+        <Route path="/driving_summary" element={<DrivingSummary />} />
+        <Route path="/feel_unwell" element={<FeelUnwell />} />
+        <Route path="/summary_question" element={<SummaryQuestion />} />
+        <Route path="/appointment_success" element={<AppointmentSuccess />} />
+        <Route path="/summary_question" element={<SummaryQuestion />} />
+        <Route path="/community_support" element={<CommunitySupport />} />
+        <Route path="/music_player" element={<MusicPlayer />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
