9527e2f4393beb7fa60632c3f7752373d9b87625
Badger-Jay <jy3287@columbia.edu>
2024-03-12 21:31:25 -0500

    Update.

--- Frontend/front/src/components/Weather/index.jsx
+++ Frontend/front/src/components/Weather/index.jsx
@@ -1,43 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function Weather() {
-    const [weatherData, setWeatherData] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
 
-    useEffect(() => {
-        const fetchWeather = async () => {
-            const url = `https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=IdbClpIoU4PJgVBF66bUSPx7o7i8egxp`;
-            console.log(url);
-            try {
-                const response = await fetch(url, {
-                    method: 'GET',
-                    headers: {
-                    }
-                });
-                const data = await response.json();
-                setWeatherData(data); 
-            } catch (error) {
-                console.error("Error fetching weather data:", error);
-                console.log("failes");
-            }
-        };
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const url = `https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=IdbClpIoU4PJgVBF66bUSPx7o7i8egxp`;
+      console.log(url);
+      try {
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {},
+        });
+        const data = await response.json();
+        setWeatherData(data);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+        console.log("failes");
+      }
+    };
 
-        fetchWeather();
-    }, []);
+    fetchWeather();
+  }, []);
 
-    return (
-        <div className="weather-container">
-            <h1>Today's weather is:</h1>
-            {weatherData ? (
-                <div>
-                    <p>Temperature: {weatherData?.data?.timelines[0]?.intervals[0]?.values?.temperature}°C</p>
-                    <p>Humility: {weatherData?.data?.timelines[0]?.intervals[0]?.values?.humility}</p>
-                </div>
-            ) : (
-                <p>Loading...</p>
-            )}
-            <Link to="/" className="back-button">Back to Main Page</Link>
+  return (
+    <div className="weather-container">
+      <h1>Today's weather is:</h1>
+      {weatherData ? (
+        <div>
+          <p>
+            Temperature:{" "}
+            {weatherData?.data?.timelines[0]?.intervals[0]?.values?.temperature}
+            °C
+          </p>
+          <p>
+            Humility:{" "}
+            {weatherData?.data?.timelines[0]?.intervals[0]?.values?.humility}
+          </p>
         </div>
-    );
+      ) : (
+        <p>Loading...</p>
+      )}
+      <Link to="/" className="back-button">
+        Back to Main Page
+      </Link>
+    </div>
+  );
 }
 export default Weather;
