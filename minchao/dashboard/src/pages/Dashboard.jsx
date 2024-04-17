61b47968d3c23d698817f61e5cecdf3f55034d9f
Minchao Huang <minchaoh2002@gmail.com>
2024-04-17 17:07:51 -0500

    Feature: Upload the new dashboard

---
+++ minchao/dashboard/src/pages/Dashboard.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+
+import Sidebar from '../partials/Sidebar';
+import WelcomeBanner from '../partials/dashboard/WelcomeBanner';
+import DashboardCard04 from '../partials/dashboard/DashboardCard04';
+import DashboardCard05 from '../partials/dashboard/DashboardCard05';
+import DashboardCard06 from '../partials/dashboard/DashboardCard06';
+import DashboardCard07 from '../partials/dashboard/DashboardCard07';
+import DashboardCard08 from '../partials/dashboard/DashboardCard08';
+import DashboardCard09 from '../partials/dashboard/DashboardCard09';
+import DashboardCard11 from '../partials/dashboard/DashboardCard11';
+import DashboardCard12 from '../partials/dashboard/DashboardCard12';
+import DashboardCard13 from '../partials/dashboard/DashboardCard13';
+
+function Dashboard() {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="flex h-screen overflow-hidden">
+
+      {/* Sidebar */}
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+      {/* Content area */}
+      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+
+        <main>
+          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+
+            {/* Welcome banner */}
+            <WelcomeBanner />
+
+            {/* Dashboard actions */}
+            <div className="sm:flex sm:justify-between sm:items-center mb-8">
+            </div>
+
+            {/* Cards */}
+            <div className="grid grid-cols-12 gap-6">
+              {/* Bar chart (Direct vs Indirect) */}
+              <DashboardCard04 />
+              {/* Line chart (Real Time Value) */}
+              <DashboardCard05 />
+              {/* Doughnut chart (Top Countries) */}
+              <DashboardCard06 />
+              {/* Table (Top Channels) */}
+              <DashboardCard07 />
+              {/* Line chart (Sales Over Time) */}
+              <DashboardCard08 />
+              {/* Stacked bar chart (Sales VS Refunds) */}
+              <DashboardCard09 />
+              {/* Card (Reasons for Refunds) */}
+              <DashboardCard11 />
+              {/* Card (Recent Activity) */}
+              <DashboardCard12 />
+              {/* Card (Income/Expenses) */}
+              <DashboardCard13 />
+              
+            </div>
+
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
